142fb68c6967e254b67b6c1103248c183c400d35
caleb <caleb@cashboxng.com>
2020-02-27 11:47:17 +0100

    App navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducers from "./Reducers/index.js";
+import { applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+import Header from './Components/Header.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Shop from './Pages/Shop.js';
+import Contact from './Pages/Contact.js';
+import Home from './Pages/Home.js';
+
+
+let store = createStore(
+  allReducers,
+  applyMiddleware(thunk)
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+              <Shop />
+          </Route>
+
+          <Route path="/contact">
+              <Contact />
+          </Route>
+
+          <Route path="/">
+              <Home />
+          </Route>
+          
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
